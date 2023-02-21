927ecf27e02704680ae5583e0a62e3769503b59b
mesciuS <giovadev@gmail.com>
2023-02-21 14:49:24 +0100

    feat: esercizio JS

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// chiede all'utente nome, cognome e colore preferito. Poi aggiungere un numero a caso
+
+/* 
+-chiedere all'utente il suo nome
+-chiedere all'utente il suo cognome
+-chiedere all'utente il suo colore preferito
+-memorizzare tutto in delle variabili
+-stampare in pagina il risultato combinato */
+
+let userName = prompt("Come ti chiami?")
+
+let userSurname = prompt("Come ti cognomini?")
+
+let userColor = prompt("Qual è il tuo colore preferito?")
+
+document.writeln(`${userName}${userSurname}${userColor}3`)
